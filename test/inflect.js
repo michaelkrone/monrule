ff4a4e25b4e2b5137d43d1ed02a2ceb14be747fa
Michael Krone <mk@gastro-instruments.de>
2016-03-11 00:29:37 +0100

    test(inflect): add deep equality tests

--- test/inflect.js
+++ test/inflect.js
@@ -4,15 +4,18 @@ import test from 'ava';
 import {inflect} from '../lib/utils';
 
 test.beforeEach(t => {
+	t.context.theObject = {unique: true};
+	
 	const keymap = {
-		Name: 'name',
-		Age: 'age',
+		aName: 'name',
+		anObject: 'object',
 		missing: 'missing'
 	};
 
 	t.context.remapped = {
-		Name: 'Trevor',
-		Age: 28
+		aName: 'Trevor',
+		anAge: 42,
+		anObject: t.context.theObject
 	};
 	
 	inflect(t.context.remapped, keymap);
@@ -22,13 +25,19 @@ test('should return an object', t => {
 	t.ok(typeof t.context.remapped === 'object');
 });
 
-test('should map keys from obj->keymap', t => {
+test('should remap keys', t => {
 	t.ok(t.context.remapped.name);
-	t.ok(t.context.remapped.age);
+	t.ok(t.context.remapped.object);
+	t.ok(t.context.remapped.anAge);
 	t.notOk(t.context.remapped.Name);
-	t.notOk(t.context.remapped.Age);
 });
 
-test('should ignore missing keys', t => {
+test('should have set the proper values', t => {
+	t.same(t.context.remapped.name, 'Trevor');
+	t.same(t.context.remapped.anAge, 42);
+	t.same(t.context.remapped.object, t.context.theObject);
+});
+
+test('should not map missing keys', t => {
 	t.notOk(t.context.remapped.missing);
 });
