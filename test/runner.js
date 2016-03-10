b546599ac97122b37ec1ebe33e766734b5f85b80
Michael Krone <mk@gastro-instruments.de>
2016-03-10 23:46:58 +0100

    refactor(runner): get rid of validator

--- test/runner.js
+++ test/runner.js
@@ -2,10 +2,10 @@
 
 import test from 'ava';
 import {spy} from 'sinon';
-import {RuleRunner} from '../lib';
+import {Runner} from '../lib';
 
 test('should throw for an invalid rule config', t => {
-	const rr = new RuleRunner();	
+	const rr = new Runner();	
 	t.throws(() => rr.register());
 	t.throws(() => rr.register(null, {a: 1}));
 	t.throws(() => rr.register({rule: {}}));
@@ -13,87 +13,103 @@ test('should throw for an invalid rule config', t => {
 });
 
 test('should check a rule', async t => {
-	const rr = new RuleRunner();
+	const rr = new Runner();
 	const rule = (a, b) => a > b;
 	const ruleConf = [undefined, {key: rule, rule}];
 
-	let result = await rr.check(ruleConf, 1, 2);
-	t.false(result.valid);
+	let data = await rr.check(ruleConf, 1, 2);
+	t.false(data.result);
 	
-	result = await rr.check(ruleConf, 2, 1);
-	t.true(result.valid);
+	data = await rr.check(ruleConf, 2, 1);
+	t.true(data.result);
 });
 
 test('should register and iterate the rules', async t => {
-	const rr = new RuleRunner();	
+	const rr = new Runner();	
 	const rule = (a, b) => a > b;
 	const elur = (a, b) => a < b;
 
-	rr.register(rule, {rule});
-	rr.register(elur, {rule: elur});
+	rr.register(rule, {rule})
+		.register(elur, {rule: elur});
 
 	for (let p of rr.iterate(1, 2)) {
 		let r = await p;
 		if (r === rule) {
-			t.false(r.valid);
+			t.false(r.result);
 		}
 		
 		if (r === elur) {
-			t.true(r.valid);
+			t.true(r.result);
 		}
 	}
 	
 	for (let p of rr.iterate(2, 1)) {
 		let r = await p;
 		if (r === rule) {
-			t.true(r.valid);
+			t.true(r.result);
 		}
 		
 		if (r === elur) {
-			t.false(r.valid);
+			t.false(r.result);
 		}
 	}
 });
 
 test.cb('should emit an event on failed validation', t => {
-	const rr = new RuleRunner();
-	const rule = (a, b) => a > b;
+	const rr = new Runner();
+	const rule = (a, b) => false;
 	
-	rr.register('test', {rule});
-		
-	rr.on('test', (result) => {
-		t.false(result.valid);
+	rr.on('test', (data) => {
+		t.false(data.result);
 		t.end();
 	});
 	
-	rr.run(1, 2);
+	rr.register('test', {rule})
+		.run(1, 2);
 });
 
 test.cb('should not emit an event on suceeded validation by default', t => {
-	const rr = new RuleRunner();
-	const rule = (a, b) => a > b;
+	const rr = new Runner();
+	const rule = (a, b) => true;
 	const eventSpy = spy();
 
     rr.on('test', eventSpy);
-	rr.register('test', {rule});
-	
+		
 	setTimeout(() => {
       t.false(eventSpy.called);
       t.end();
     });
 	
-	rr.run(2, 1);
+	rr.register('test', {rule}).run(2, 1);
 });
 
 test.cb('should emit an event on suceeded validation if I told her so', t => {
-	const rr = new RuleRunner({triggerValid: true});
-	const rule = (a, b) => a > b;
+	const rr = new Runner({triggerValid: true});
+	const rule = (a, b) => true;
 	
-	rr.register('test', {rule});
-	rr.run(2, 1);
-	
-	rr.on('test', (result) => {
-		t.true(result.valid);
+	rr.on('test', (data) => {
+		t.true(data.result);
 		t.end();
-	});	
+	});
+			
+	rr.register('test', {rule}).run(2, 1);
+});
+
+test.cb('should not emit at all if the emit option is set to false ', t => {
+	const rr = new Runner({emit: false});
+	const rule = (a, b) => false;
+	const elur = (a, b) => true;
+	const eventSpy = spy();
+
+    rr.on('false', eventSpy);
+	rr.on('true', eventSpy);
+		
+	setTimeout(() => {
+      t.false(eventSpy.called);
+      t.end();
+    });
+	
+	rr.register('false', {rule})
+		.register('true', {rule: elur})
+		.run(2, 1);
 });
--- test/validator.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import test from 'ava';
-import {Validator} from '../lib';
-
-test('should do smth', t => {
-	const v = new Validator();
-	t.pass();
-});
