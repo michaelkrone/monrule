6172313da9d49ad3aa82b7c3e08641a7c6ed9034
Michael Krone <mk@gastro-instruments.de>
2016-03-17 00:06:15 +0100

    feat(object-writer): accept functions as values, refactor class name, simplify tests

--- test/object-writer.js
+++ test/object-writer.js
@@ -2,30 +2,34 @@
 
 import test from 'ava';
 import mongoose from 'mongoose';
-import {spy} from 'sinon';
 import {ObjectWriter} from '../lib';
 
 test.cb.before(t => {
 	mongoose.connect('mongodb://127.0.0.1/monrule', t.end);
 });
 
-test.beforeEach(t => {
-	t.context.e = new ObjectWriter();
-	t.context.createSpy = spy(t.context.e.model, 'create');
-})
-
-test.afterEach(t => {
-	t.context.createSpy.restore();
-})
-
-test.serial('should write an event into the database', async t => {
-	await t.context.e.write('data', 'data');
-	t.true(t.context.createSpy.calledOnce);
-	t.same(t.context.createSpy.getCall(0).args[0], {id: 'data', event: 'data'});
+test('should write an event into the database', async t => {
+	let e = new ObjectWriter();
+	let m = await e.write('data', 'data');
+	t.is(m.object, 'data');
+	t.is(m.id, 'data');
 });
 
-test.serial('should generate an id if none is given', async t => {
-	await t.context.e.write('data');
-	t.true(t.context.createSpy.calledOnce);	
-	t.true(typeof t.context.createSpy.getCall(0).args[0].id === 'string');
+test('should generate an id if none is given', async t => {
+	let e = new ObjectWriter();
+	let m = await e.write('data');
+	t.is(m.object, 'data');	
+	t.true(typeof m.id === 'string');
+});
+
+test('should accept a function as event data', async t => {
+	let e = new ObjectWriter();
+	let m = await e.write(() => 'test', 'data');	
+	t.true(m.object === 'test');
+});
+
+test('should accept a function as id', async t => {
+	let e = new ObjectWriter();
+	let m = await e.write('data', () => 'test');	
+	t.true(m.id === 'test');
 });
